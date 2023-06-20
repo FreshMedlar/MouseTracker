dfecf2f890ad4652328f7c6a2093cb189219fbc0
FreshMedlar <segatori.1932373@studenti.uniroma1.it>
2023-06-20 19:35:20 +0200

    modello tutto sbagliato,aggiunto mousePlotWithList

--- plotter.py
+++ plotter.py
@@ -30,4 +30,32 @@ def mousePlot(file):
     print("Disegno finito")
     plt.show
 
-mousePlot("aim_data/sample8.txt")
+def mousePlotWithList(lis):
+    # plot
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, 1920)
+    ax.set_ylim(0, 1080)  
+    plt.draw()
+
+    mouse_data = lis
+
+    x1, y1 = mouse_data[0][0], mouse_data[0][1]
+    print(x1, y1)
+
+    ax.plot(x1, y1, 'ro')
+
+    print("Inizio disegno plot")
+    for row in mouse_data[1:]:
+        x2,y2 = row[0], row[1]
+        print(f"Point {x1},{y1} to {x2},{y2}")
+        
+        plt.pause(3/100)
+        ax.plot([x1, x2], [y1, y2], 'b-')
+        x1, y1 = x2, y2
+
+    print("Disegno finito")
+    plt.show
+
+
+
+#mousePlot("aim_data/sample8.txt")
